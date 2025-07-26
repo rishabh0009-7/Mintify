a35d5ad47e47f7ea26a044eeb74726a84939f71c
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-07-26 19:37:28 +0530

    token creation

--- utils/Tokencreation.ts
+++ utils/Tokencreation.ts
@@ -1,56 +1,101 @@
-import { PublicKey , Keypair , Connection, Transaction, SystemProgram, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useWallet } from '@solana/wallet-adapter-react';
+import { 
+    Connection, 
+    Keypair, 
+    Transaction,
+    SystemProgram,
+    PublicKey,
+    Connection
+  } from '@solana/web3.js';
 
-import {
-   createInitializeMintInstruction,
-   createAssociatedTokenAccountInstruction,
-   createMintToInstruction,
-   getMinimumBalanceForRentExemptMint,
-   MINT_SIZE,
-   TOKEN_PROGRAM_ID,
-   getAssociatedTokenAddress
+  import {
+    createInitializeMintInstruction,
+    createAssociatedTokenAccountInstruction,
+    createMintToInstruction,
+    getMinimumBalanceForRentExemptMint,
+    MINT_SIZE,
+    TOKEN_PROGRAM_ID,
+    getAssociatedTokenAddress,
+    createAccount,
    
+  } from '@solana/spl-token';
+
+
+  import {
+    createCreateMetadataAccountV3Instruction,
+    PROGRAM_ID as METADATA_PROGRAM_ID,
+  } from '@metaplex-foundation/mpl-token-metadata';
+import { connect } from 'http2';
+import { transcode } from 'buffer';
+
+
+//   create token
+
+const createToken  = async (wallet:any ){
+const connection = new Connection("https://api.devnet.solana.com', 'confirmed")
+
+try {
+
+    console.log("started creating token ....")
+
+    // 1- generate mintkeypair 
+    const mintKeypair = Keypair.generate ()
+    
+    // 2-get rent amount 
+    const lamport = await getMinimumBalanceForRentExemptMint(connection)
+
+    // 3- get  associatedToken address
+
+    const associatedTokenAddress = await getAssociatedTokenAddress(
+        mintKeypair.publicKey,
+        wallet.publicKey
+
+    )
+    
+    
+    // 4- crete metadata address 
+    const [metadataAddress]  = PublicKey.findProgramAddressSync(
+        [
+            Buffer.from ("metadata "),
+            METADATA_PROGRAM_ID.toBuffer(),
+            mintKeypair.publicKey.toBuffer()
+        ], 
+        METADATA_PROGRAM_ID
+    )
+
+    // 5- build transaction 
+
+    const transaction = new Transaction()
+
+// i)create mint account 
+    transaction.add (
+        SystemProgram.createAccount({
+            fromPubkey :wallet.publicKey,
+            newAccountPubkey : mintKeypair.publicKey,
+            space :MINT_SIZE,
+            lamports,
+            programId :TOKEN_PROGRAM_ID
+
+        })
+
+
+
+    )
+
+
+    // ii)initialise mint 
+
+    transaction.add(
+        createInitializeMintInstruction(
+            mintKeypair.publicKey,
+            parseInt(tokenData.decimals ),
+            wallet.publicKey
+
+        )
+    )
+    
+} catch (error) {
     
 }
 
-from 
-"@solana/spl-token"
-
-import {
-    createCreateMetadataAccountV3Instruction,
-    PROGRAM_ID as METADATA_PROGRAM_ID,
-  }
-   from '@metaplex-foundation/mpl-token-metadata';
-
-
-// initialise connection to solana network 
-   const connection = new Connection('https://api.devnet.solana.com' , "confirmed ")
-
-
-
-//    function to create  spl token 
- export const createSpl = async (Wallet:any , connection:any)=>{
-
-    try {
-        console.log('creating spl token ......')
-
-
-        // 1- generate a mint key pair
-        const mintKeypair = Keypair.generate()
-
-        // 2- calculate required rent 
-        const lamports = await getMinimumBalanceForRentExemptMint(connection)
-        
-
-        // 3 - get assosciated token address
-        const associatedTokenAddress = await getAssociatedTokenAddress(
-            mintKeypair.publicKey,
-            Wallet.publicKey
-
-        )
-
-
-    } catch (error) {
-        
-    }
-
- }
+}
